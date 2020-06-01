7fa3c32a3cdc7109c5addb19cfe3c8f752e1077f
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-06-01 18:34:13 +0700

    Add an endpoint to show all genres

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const Joi = require('joi');
-
 const app = express();
-
 const PORT = 3000;
+
+app.use(express.json());
 
 const genres = [
     { id: 1, name: 'Action' },
     { id: 2, name: 'Horror' },
     { id: 3, name: 'Romance' },
 ];
+
+app.get('/api/genres', (req, res) => {
+    res.send(genres);
+});
 
 app.listen(PORT, () => {
     console.log(`The server running on: ${PORT}...`);
