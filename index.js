8ac227152e64a112a0ad38025effc4f8d1d8f0a0
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-06-01 18:30:43 +0700

    Add the list of genres

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const app = express();
 
 const PORT = 3000;
 
+const genres = [
+    { id: 1, name: 'Action' },
+    { id: 2, name: 'Horror' },
+    { id: 3, name: 'Romance' },
+];
+
 app.listen(PORT, () => {
     console.log(`The server running on: ${PORT}...`);
 })
