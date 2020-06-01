a7dc51ee287dd81e2e8063ab010d7199038ebcc4
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-06-01 18:43:09 +0700

    Add endpoint to get single genre

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ app.get('/api/genres', (req, res) => {
     res.send(genres);
 });
 
+app.get('/api/genres/:id', (req, res) => {
+    const genre = genres.find(c => c.id == parseInt(req.params.id));
+    if (!genre) return res.status(404).send('The genre with the given ID was not found.');
+    res.send(genre);
+});
+
 app.listen(PORT, () => {
     console.log(`The server running on: ${PORT}...`);
 })
